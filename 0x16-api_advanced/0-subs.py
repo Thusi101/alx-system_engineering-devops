e8c1d9a80158a1badc14c56acd2bab216f326b32
Thusi101 <moraseroka10@gmail.com>
2024-06-18 19:41:04 -0700

    getrightsub

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,16 +3,15 @@
 number of subscribers for a given subreddit. """
 
 import requests
-headers = {"User-Agent": "ubuntu:alx:v1.0 (by /u/Thusi101_)"}
+headers = {"User-Agent": "ubuntu:alx:v1.0 (by /u/Thusi101)"}
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    user_agent = "person_at_school"
+    header = {'User-Agent': user_agent}
+    resp = requests.get(url, headers=header, allow_redirects=False)
+    if (resp.status_code != 200):
         return 0
+    else:
+        return(resp.json()['data']['subscribers'])
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-""" Queries the Reddit API and prints the
-titles of the first 10 hot posts listed for a given subreddit. """
-
-import requests
-from sys import argv
+""" Module to request subreddit hot posts """
 
 
 def top_ten(subreddit):
-    '''
-        returns the top ten posts for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
-    try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
-    except Exception:
-        print(None)
+    """ Return the number hot posts """
+    import requests
 
-
-if __name__ == "__main__":
-    top_ten(argv[1])
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    user_agent = "person_at_school"
+    header = {'User-Agent': user_agent}
+    resp = requests.get(url, headers=header, allow_redirects=False,
+                        params={'limit': 10})
+    if (resp.status_code != 200):
+        print('None')
+    else:
+        data = resp.json()["data"]
+        children = data['children']
+        for entry in children:
+            print(entry['data']['title'])
